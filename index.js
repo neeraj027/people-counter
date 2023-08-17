0763932febd091f5716352861488c4744909afa8
neeraj027 <neerajkshatriy@gmail.com>
2023-08-17 19:19:38 +0530

    added secondary code to index.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const count = document.querySelector(".count")
-// const btn = document.querySelector(".add")
-// let temp = 0;
+const btn = document.querySelector(".add")
+let temp = 0;
 
-// btn.addEventListener("click",function(){
-//     temp+=1;
-//     count.textContent=temp;
-// })
-
-temp = 0
-function increment(){
+btn.addEventListener("click",function(){
     temp+=1;
     count.textContent=temp;
-    console.log("click")
-}
+})
+
+//or you can use below code
+
+// const count = document.querySelector(".count")
+// temp = 0
+// function increment(){
+//     temp+=1;
+//     count.textContent=temp;
+// }

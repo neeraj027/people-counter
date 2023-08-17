1c4ffec08ec66cc011bfac14445dc21cdb993ae1
neeraj027 <neerajkshatriy@gmail.com>
2023-08-17 12:52:15 +0530

    index.html and index.js created

---
+++ index.js
@@ -0,0 +1,15 @@
+const count = document.querySelector(".count")
+// const btn = document.querySelector(".add")
+// let temp = 0;
+
+// btn.addEventListener("click",function(){
+//     temp+=1;
+//     count.textContent=temp;
+// })
+
+temp = 0
+function increment(){
+    temp+=1;
+    count.textContent=temp;
+    console.log("click")
+}
